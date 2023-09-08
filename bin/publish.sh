f63b2442ae1edc29e3be3d846e91de24a5c82512
Herman Seip <jensse@duck.com>
2023-09-08 08:33:02 +0000

    created marfile to contain all

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 marktag="9.9.0"
 
+touch .markfile
+chmod 700 .markfile
+echo "${MARKFILE}" > .markfile
+
 function summary(){
   echo "Summary: installing mark version; ${1} testing..."
   echo $(bin/mark --version)
