27d95ab8e719d90cb2fb1836ac0913176ca13e11
Herman Seip <herman.seip@domstol.no>
2023-09-12 11:03:27 +0200

    works on my computer

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 marktag="9.9.0"
+MARKBIN=mark
 
-touch .markfile
-chmod 700 .markfile
-echo "${MARKFILE}" > .markfile
 
 function summary(){
   echo "Summary: installing mark version; ${1} testing..."
-  echo $(mark --version)
+  if [ -f ${MARKBIN} ] ; then
+    echo $(${MARKBIN} --version)
+  else
+    echo "ERROR Cant find MARKBIN at: ${MARKBIN}"
+  fi
   # echo $(env)
 }
 
@@ -21,7 +23,7 @@ function createMarkfileIfNotExist (){
       MARKFILE="${HOME}/.config/mark"
   else
     #Github actions
-    echo "Create makefile"
+    echo "Create markfile"
     touch .markfile
     chmod 700 .markfile
     echo "${MARKFILE}" > .markfile
@@ -31,21 +33,14 @@ function createMarkfileIfNotExist (){
 # Test if mark is installed, if not: install mark
 function installMark(){
   echo "Install mark if dont exist"
-  tag=${1}
-  if [ -f "bin/mark"]; then
-    echo "bin/mark, exist; Setting alias"
-    alias mark="bin/mark"
-
-  elif [[ $(mark --version) == "mark version 9*" ]]; then
-    echo "Mark is installed locally, and on path"
-
-  else
-    echo "no mark installed; installing and setting alais for excecution"
+  if [ ! type "mark" > /dev/null ]; then
+    # install mark
+    echo "Mark not installed; installing and setting alais for excecution"
     wget https://github.com/kovetskiy/mark/releases/download/${tag}/mark_Linux_x86_64.tar.gz
     tar -xzvf mark_Linux_x86_64.tar.gz -C bin/
-    chmod 755 bin/mark
-    alias mark="bin/mark"
+    PATH=bin:${PATH}
   fi
+
 }
 
 function publishMd(){
@@ -54,7 +49,7 @@ function publishMd(){
   # echo "PWD: $(pwd)"
   # echo $(env)
   echo "Search for files in: $(pwd)"
-  find "$(pwd)" -name "*.md" -path "*src/202*" -exec mark --config ${MARKFILE} -f {} \;
+  find "$(pwd)" -name "*.md" -path "*src/202*" -exec ${MARKBIN} --config ${MARKFILE} -f {} \;
 
   #echo $(mark --config ${MARKFILE} -f ${GITHUB_WORKSPACE}/src/*.md)
 }
